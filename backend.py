727e079d7e5fddb8a9fdbb8e5258eaacf59d58a8
steventio <101958689+Steven-Tio@users.noreply.github.com>
2023-10-13 16:44:29 -0400

    RD10

--- backend.py
+++ backend.py
@@ -18,6 +18,8 @@ class User(BaseModel):
     @field_validator('pid')
     def pid_must_be_9_digits(cls, value: int):
         # TODO: Raise ValueError if pid is not 9 digits
+        if(len(str(value)) != 9):
+            raise ValueError("Invalid PID")
         return value
 
 
@@ -41,6 +43,13 @@ class UserEntity(Base):
     last_name: Mapped[str] = mapped_column(String)
 
     # TODO: Define from_model and to_model methods
+    @classmethod
+    def from_model(cls: Type[Self], user: User) -> Self:
+        return UserEntity(pid=user.pid, first_name=user.first_name, last_name = user.last_name)
+    
+    # TODO: Define an *instance method* to convert a UserEntity into a Pydantic User model
+    def to_model(self) -> User:
+        return User(pid=self.pid, first_name=self.first_name, last_name=self.last_name)
 
 
 Base.metadata.create_all(engine)
@@ -57,7 +66,10 @@ class UserService:
 
     def register(self, user: User) -> User:
         # TODO: Persist the `user` object
-        return user
+        user_entity: UserEntity = UserEntity.from_model(user)
+        self._session.add(user_entity)
+        self._session.commit()
+        return user_entity.to_model()
 
     def get(self, pid: int) -> User:
         user = self._session.get(UserEntity, pid)
@@ -70,6 +82,10 @@ class UserService:
 # =========================
 # Demo
 # 1. Construct a User
+a_user = User(pid=123456789, first_name="Kris", last_name="Jordan")
 # 2. Construct a UserService(session_factory())
+user_service = UserService(session_factory)
 # 3. Register your User object using the service
+user_service.register(a_user)
 # 4. Use the get method of your service to fetch the user from database
+print(user_service.get(123456789))
--- notes.py
+++ notes.py
@@ -1,2 +1,53 @@
 # Use this file for notes and running examples...
 # As expected, run it with `python3 -m notes`
+
+from sqlalchemy import create_engine
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+#print(engine)
+
+from sqlalchemy import text
+
+with engine.connect() as conn:
+    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 1, "y": 1}, {"x": 2, "y": 4}]
+    )
+    conn.commit()
+
+with engine.begin() as conn:
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 6, "y": 8}, {"x": 9, "y": 10}],
+    )
+
+#with engine.connect() as conn:
+#    result = conn.execute(text("select x, y from some_table"))
+#    for x, y in result:
+#        print(f"x: {x}  y: {y}")
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT x, y FROM some_table WHERE y > :y"), {"y": 4})
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
+
+from sqlalchemy.orm import Session
+
+stmt = text("SELECT x, y FROM some_table WHERE y > :y ORDER BY x, y")
+with Session(engine) as session:
+    result = session.execute(stmt, {"y": 6})
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
+
+with Session(engine) as session:
+    result = session.execute(
+        text("UPDATE some_table SET y=:y WHERE x=:x"),
+        [{"x": 9, "y": 11}, {"x": 13, "y": 15}],
+    )
+    session.commit()
+
+stmt = text("SELECT x, y FROM some_table WHERE y > :y ORDER BY x, y")
+with Session(engine) as session:
+    result = session.execute(stmt, {"y": 6})
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
